19c4f4d7ed23614e6a5a39e5496bb7fdb3abc658
lucassilva996 <lucassilva996@outlook.com>
2022-05-09 01:13:29 -0300

    Criando entidade

---
+++ CadastroImuno/Models/Imunizante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroImuno.Models
+{
+    [Table("Imunizante")]
+    public class Imunizante
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Fabricante { get; set; }
+        [Required]
+        public DateTime AnoLote { get; set; }
+    }
+}
